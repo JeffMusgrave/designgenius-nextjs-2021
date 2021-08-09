61ba608190091c63f3d769f9b5e13ffcfd0bf240
Jeff <jeff.musgrave@gmail.com>
2021-08-09 17:33:37 -0400

    acced font weight and bolded contact me

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends NextDocument {
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&family=Paytone+One&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,900;1,300;1,400;1,600;1,900&family=Paytone+One&display=swap"
           rel="stylesheet"
         />
         <link
