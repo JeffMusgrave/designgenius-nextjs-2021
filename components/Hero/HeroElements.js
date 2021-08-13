2c6beb0515a7baf51e74d5a0ad54e9e2acc8cdac
Jeff <jeff.musgrave@gmail.com>
2021-08-13 12:53:20 -0400

    adjusted hero section for better mobile experience

--- components/Hero/HeroElements.js
+++ components/Hero/HeroElements.js
@@ -7,15 +7,15 @@ export default function HeroElements() {
         <Heading
           as="h1"
           fontSize={["34pt", "50pt", "clamp(60pt,  3.5vw, 80pt)"]}
-          mb="2.5rem"
+          mb={{ base: "1rem", lg: "2.5rem" }}
           lineHeight="1em"
           fontWeight="300"
         >
           Inspired Design
         </Heading>
         <Text
-          marginBottom="1rem"
-          fontSize={{ base: "xl", md: "3xl" }}
+          mb={{ base: "0rem", lg: "1rem" }}
+          fontSize={{ base: "2xl", md: "3xl" }}
           lineHeight={{ base: 6, md: 8 }}
           fontWeight="600"
           letterSpacing="tight"
